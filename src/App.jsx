585428458e178d3bc4aac30c4ed45271c9e6ba9c
adrianhdez2 <cesarahh3@gmail.com>
2024-03-17 16:36:20 -0600

    Upload Files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      
+      <h1>Juchi Market 🛒🍕</h1>
+      
+      <p className="read-the-docs">
+        Pronto estará disponible.
+      </p>
+    </>
+  )
+}
+
+export default App
