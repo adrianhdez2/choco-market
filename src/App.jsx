dfd95c13c0df3c1785abdc756432e91e2060dcfb
adrianhdez2 <cesarahh3@gmail.com>
2024-03-22 20:10:00 -0600

    Add media queries

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,6 @@ function App() {
 
   return (
     <>
-      
-      <h1>Juchi Market 🛒🍕</h1>
-      
-      <p className="read-the-docs">
-        Pronto estará disponible.
-      </p>
       <Footer />
     </>
   )
