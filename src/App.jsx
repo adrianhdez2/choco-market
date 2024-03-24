de99d960d681b1ce15a310894f7fcefc94597231
adrianhdez2 <cesarahh3@gmail.com>
2024-03-23 22:30:42 -0600

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-
+import { Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Error from './pages/Error'
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <Routes>
+        <Route path='/' Component={Home} />
+        <Route path='/login' Component={Login}/>
+        <Route path='/signup' Component={SignUp}/>
+        <Route path='*' Component={Error}/>
+      </Routes>
+
       <Footer />
     </>
   )
