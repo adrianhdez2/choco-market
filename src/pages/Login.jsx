f5efa805151eb414b865a4784743f993c629145b
adrianhdez2 <cesarahh3@gmail.com>
2024-03-27 14:09:52 -0600

    Fix Order tags

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,26 @@
 
 function Login() {
   return (
-    <div className="containerlogin">
-      <h1>Iniciar sesión</h1>
-      <form id="login-form">
-        <input type="text" id="matricula" name="matricula" placeholder="Ingrese su matrícula" required />
-        <input type="password" id="password" name="password" placeholder="Ingrese su contraseña" required />
-        <p id="forgot-password">Olvidé mi contraseña</p>
-        <button type="submit">Entrar</button>
-      </form>
-      <p>¿No tienes cuenta? <span id="signup-link">Registrarme</span></p>
+    <div className="container_login">
+      <div className="login">
+        <h3 className="title">Iniciar sesión</h3>
+        <form id="login-form">
+          <div className="form_input_user form_input">
+            <span className="form_input_icon">O</span>
+            <input className="input" type="text" id="matricula" name="matricula" placeholder="Ingrese su matrícula" required />
+          </div>
+          <div className="form_input_password form_input">
+            <span className="form_input_icon">O</span>
+            <input className="input password" type="password" id="password" name="password" placeholder="Ingrese su contraseña" required />
+            <span className="form_input_icon show_pass">O</span>
+          </div>
+          <a href="#" id="forgot_password">Olvidé mi contraseña</a>
+          <button className="btn btn_primary" type="submit">Entrar</button>
+        </form>
+        <p className="not_account">¿No tienes cuenta?
+          <a href="#" id="signup_link">Registrarme</a>
+        </p>
+      </div>
     </div>
   )
 }
