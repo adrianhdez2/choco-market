6e4aa5ed46c3dcb26b5b1e6043678fbff3f16a39
adrianhdez2 <cesarahh3@gmail.com>
2024-03-27 14:16:13 -0600

    Add enter spaces

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,15 +5,18 @@ function Login() {
       <div className="login">
         <h3 className="title">Iniciar sesión</h3>
         <form id="login-form">
+          
           <div className="form_input_user form_input">
             <span className="form_input_icon">O</span>
             <input className="input" type="text" id="matricula" name="matricula" placeholder="Ingrese su matrícula" required />
           </div>
+
           <div className="form_input_password form_input">
             <span className="form_input_icon">O</span>
             <input className="input password" type="password" id="password" name="password" placeholder="Ingrese su contraseña" required />
             <span className="form_input_icon show_pass">O</span>
           </div>
+
           <a href="#" id="forgot_password">Olvidé mi contraseña</a>
           <button className="btn btn_primary" type="submit">Entrar</button>
         </form>
