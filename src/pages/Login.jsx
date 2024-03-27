1549c0a2bcd1c90bdff46c18a83cb5d96523c9c7
Antonio De Jesus Loya Castillo <122246049+AntonioLoya@users.noreply.github.com>
2024-03-27 02:41:38 -0600

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,16 @@
 
 function Login() {
   return (
-    <section>
-      <h2>Login</h2>
-    </section>
+<div className="container">
+      <h1>Iniciar sesión</h1>
+      <form id="login-form">
+        <input type="text" id="matricula" name="matricula" placeholder="Ingrese su matrícula" required />
+        <input type="password" id="password" name="password" placeholder="Ingrese su contraseña" required />
+        <p id="forgot-password">Olvidé mi contraseña</p>
+        <button type="submit">Entrar</button>
+      </form>
+      <p>¿No tienes cuenta? <span id="signup-link">Registrarme</span></p>
+    </div>
   )
 }
 
