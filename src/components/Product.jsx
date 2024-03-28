dec1f9837e45d7ccb620bc1064e07217b1c0abcc
adrianhdez2 <cesarahh3@gmail.com>
2024-03-27 23:30:13 -0600

    Create component product (card)

---
+++ src/components/Product.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom"
+
+
+function Product({product}) {
+  const {id, name, descrip, img, stars, price} = product
+  let newDescrip = descrip.trim().length > 70 ? descrip.substring(0, 70) + "..." : descrip
+
+  return (
+    <Link to={`/products/${id}`} className="card_product">
+      <div className="card_product_img_container">
+        <img className="card_product_img" src={`/${img}`} alt="" />
+        <span className="card_product_stars">
+          <span>&#9733;</span>
+          {stars}
+        </span>
+      </div>
+      <div className="card_product_details_container">
+        <h4 className="card_product_title">{name}</h4>
+        <p className="card_product_description">{newDescrip}</p>
+      </div>
+      <div className="card_product_price_container">
+        <span className="card_product_price">$ {price}</span>
+      </div>
+    </Link>
+  )
+}
+
+export default Product
