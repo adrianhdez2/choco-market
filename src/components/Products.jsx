0fddcaa36e3446c2b112eb593e9eb9aed39c71a9
adrianhdez2 <cesarahh3@gmail.com>
2024-03-27 23:30:34 -0600

    Create component products

---
+++ src/components/Products.jsx
@@ -0,0 +1,99 @@
+import Product from './Product'
+
+const products = [
+  {
+    "id": 1,
+    "name": "Mouse",
+    "img": "profile.jpg",
+    "stars": "3.5",
+    "price": "19.99",
+    "descrip": "Un ratón ergonómico y preciso para facilitar la navegación y el trabajo en la computadora."
+  },
+  {
+    "id": 2,
+    "name": "Reglas",
+    "img": "profile.jpg",
+    "stars": "3.5",
+    "price": "14.99",
+    "descrip": "Conjunto de reglas de diferentes longitudes, ideales para trazar líneas y mediciones precisas en proyectos escolares o de oficina."
+  },
+  {
+    "id": 3,
+    "name": "Libreta",
+    "img": "profile.jpg",
+    "stars": "1.0",
+    "price": "12.99",
+    "descrip": "Una libreta con tapa dura perfecta para tomar notas, hacer bocetos o escribir pensamientos."
+  },
+  {
+    "id": 4,
+    "name": "Bolígrafo",
+    "img": "profile.jpg",
+    "stars": 2,
+    "price": 16.99,
+    "descrip": "Un bolígrafo de tinta suave y duradera, ideal para escribir de forma cómoda y sin esfuerzo."
+  },
+  {
+    "id": 5,
+    "name": "Lápiz",
+    "img": "profile.jpg",
+    "stars": 3.5,
+    "price": 9.99,
+    "descrip": "Un lápiz de grafito resistente y de alta calidad, perfecto para dibujar, tomar notas o hacer esbozos."
+  },
+  {
+    "id": 6,
+    "name": "Papel",
+    "img": "profile.jpg",
+    "stars": 3,
+    "price": 17.99,
+    "descrip": "Un paquete de papel de alta blancura y suavidad, ideal para imprimir documentos, notas o dibujos."
+  },
+  {
+    "id": 7,
+    "name": "Cinta Adhesiva",
+    "img": "profile.jpg",
+    "stars": 3.5,
+    "price": 13.99,
+    "descrip": "Una cinta adhesiva transparente y resistente, perfecta para envolver regalos, sellar sobres o reparaciones rápidas."
+  },
+  {
+    "id": 8,
+    "name": "Cuaderno",
+    "img": "profile.jpg",
+    "stars": 5,
+    "price": 15.99,
+    "descrip": "Un cuaderno de tapa dura y papel de calidad, ideal para tomar apuntes en clase, hacer listas o llevar un diario."
+  },
+  {
+    "id": 9,
+    "name": "Calculadora",
+    "img": "profile.jpg",
+    "stars": 4,
+    "price": 29.99,
+    "descrip": "Una calculadora con funciones básicas y avanzadas, perfecta para cálculos matemáticos en la escuela, el trabajo o en casa."
+  },
+  {
+    "id": 10,
+    "name": "Tijeras",
+    "img": "profile.jpg",
+    "stars": 3.5,
+    "price": 11.99,
+    "descrip": "Un par de tijeras de acero inoxidable con hojas afiladas y mango ergonómico, ideales para cortar papel, tela o manualidades."
+  }
+]
+
+
+function Products() {
+  return (
+    <section id="products">
+      {
+        products.map((item) => (
+          <Product key={item.id} product={item} />
+        ))
+      }
+    </section>
+  )
+}
+
+export default Products
