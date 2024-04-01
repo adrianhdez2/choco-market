76b96dbc8f69f278e87e4cccc57da04ace605956
adrianhdez2 <cesarahh3@gmail.com>
2024-03-31 20:24:43 -0600

    Update error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom"
 
 
 function Error({ error = 'Está página no existe' }) {
   return (
     <section id="error_page">
-      <h2>Error 404, {error}</h2>
+      <div className="container_error_page">
+        <div>
+            <span className="error_4">4</span>
+            <span className="error_4">0</span>
+            <span className="error_4">4</span>
+        </div>
+        <p className="description_error_page">{error}</p>
+        <Link to={"/"} className="btn_home_error_page">Ir a inicio</Link>
+      </div>
     </section>
   )
 }
