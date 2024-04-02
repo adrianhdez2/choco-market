7f77ffdbd0449ea85e532f243a612431f9a5e9f7
adrianhdez2 <cesarahh3@gmail.com>
2024-04-01 20:47:50 -0600

    Create nav component

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+function Nav() {
+    return (
+        <nav className='header_nav'>
+            <div className='header_nav_search'>
+                <input type="text" name="search" id="search" placeholder='Botanitas, pastel, dulces...' autoComplete='off' />
+                <button type='button' className='header_nav_btnSearch'>B</button>
+            </div>
+            <div className='header_nav_filters'>
+                <Link className="nav_filter" to={"/search?q=comida"}>Comida</Link>
+                <Link className="nav_filter" to={"/search?q=bebidas"}>Bebidas</Link>
+                <Link className="nav_filter" to={"/search?q=accesorios"}>Accesorios</Link>
+                <Link className="nav_filter" to={"/search?q=dulceria"}>Dulceria</Link>
+                <Link className="nav_filter" to={"/search?q=postres"}>Postres</Link>
+                <Link className="nav_filter" to={"/search?q=otros"}>Más</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
