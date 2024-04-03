c8c63dbbd264bf4113dbcad336d22209e6d05b6d
adrianhdez2 <cesarahh3@gmail.com>
2024-04-02 18:36:36 -0600

    Add new details

--- src/constans/data.js
+++ src/constans/data.js
@@ -4,6 +4,7 @@ export const dataProducts = [
     "name": "Mouse",
     "img": "profile.jpg",
     "stars": 3.5,
+    "stock": 10,
     "price": 19.99,
     "id_unico": "A1B2C3D4E5",
     "descrip": "Un ratón ergonómico y preciso para facilitar la navegación y el trabajo en la computadora."
@@ -13,6 +14,7 @@ export const dataProducts = [
     "name": "Reglas",
     "img": "profile.jpg",
     "stars": 3.5,
+    "stock": 30,
     "price": 14.99,
     "id_unico": "F6G7H8I9J0",
     "descrip": "Conjunto de reglas de diferentes longitudes, ideales para trazar líneas y mediciones precisas en proyectos escolares o de oficina."
@@ -22,6 +24,7 @@ export const dataProducts = [
     "name": "Libreta",
     "img": "profile.jpg",
     "stars": 1.0,
+    "stock": 50,
     "price": 12.99,
     "id_unico": "K1L2M3N4O5",
     "descrip": "Una libreta con tapa dura perfecta para tomar notas, hacer bocetos o escribir pensamientos."
@@ -31,6 +34,7 @@ export const dataProducts = [
     "name": "Bolígrafo",
     "img": "profile.jpg",
     "stars": 2.0,
+    "stock": 100,
     "price": 16.99,
     "id_unico": "P6Q7R8S9T0",
     "descrip": "Un bolígrafo de tinta suave y duradera, ideal para escribir de forma cómoda y sin esfuerzo."
@@ -40,6 +44,7 @@ export const dataProducts = [
     "name": "Lápiz",
     "img": "profile.jpg",
     "stars": 3.5,
+    "stock": 87,
     "price": 9.99,
     "id_unico": "U1V2W3X4Y5",
     "descrip": "Un lápiz de grafito resistente y de alta calidad, perfecto para dibujar, tomar notas o hacer esbozos."
@@ -49,6 +54,7 @@ export const dataProducts = [
     "name": "Papel",
     "img": "profile.jpg",
     "stars": 3.0,
+    "stock": 19,
     "price": 17.99,
     "id_unico": "Z6A7B8C9D0",
     "descrip": "Un paquete de papel de alta blancura y suavidad, ideal para imprimir documentos, notas o dibujos."
@@ -58,6 +64,7 @@ export const dataProducts = [
     "name": "Cinta Adhesiva",
     "img": "profile.jpg",
     "stars": 3.5,
+    "stock": 70,
     "price": 13.99,
     "id_unico": "E1F2G3H4I5",
     "descrip": "Una cinta adhesiva transparente y resistente, perfecta para envolver regalos, sellar sobres o reparaciones rápidas."
@@ -67,6 +74,7 @@ export const dataProducts = [
     "name": "Cuaderno",
     "img": "profile.jpg",
     "stars": 5.0,
+    "stock": 80,
     "price": 15.99,
     "id_unico": "J6K7L8M9N0",
     "descrip": "Un cuaderno de tapa dura y papel de calidad, ideal para tomar apuntes en clase, hacer listas o llevar un diario."
@@ -76,6 +84,7 @@ export const dataProducts = [
     "name": "Calculadora",
     "img": "profile.jpg",
     "stars": 4.0,
+    "stock": 50,
     "price": 29.99,
     "id_unico": "O1P2Q3R4S5",
     "descrip": "Una calculadora con funciones básicas y avanzadas, perfecta para cálculos matemáticos en la escuela, el trabajo o en casa."
@@ -85,6 +94,7 @@ export const dataProducts = [
     "name": "Tijeras",
     "img": "profile.jpg",
     "stars": 3.5,
+    "stock": 30,
     "price": 11.99,
     "id_unico": "T6U7V8W9X0",
     "descrip": "Un par de tijeras de acero inoxidable con hojas afiladas y mango ergonómico, ideales para cortar papel, tela o manualidades."
