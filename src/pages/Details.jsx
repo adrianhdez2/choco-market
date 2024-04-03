1f52e2cfdd020ebff15aea0730e3d2e97564bd3f
adrianhdez2 <cesarahh3@gmail.com>
2024-04-02 19:46:25 -0600

    Change 'true' to true

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -102,7 +102,7 @@ function Details() {
 
                     <div className="details_container_buttons">
                         <button type="button" className="details_btn_buy btn btn_primary">Comprar</button>
-                        <button type="button" disabled={isLoading ? 'true' : undefined} onClick={addCartitem} className="details_btn_cart btn btn_secondary">
+                        <button type="button" disabled={isLoading ? true : undefined} onClick={addCartitem} className="details_btn_cart btn btn_secondary">
                             {
                                 isLoading ?
                                     <div className="loading"></div>
