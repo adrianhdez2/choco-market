7eca9a230830a1c23211d490467b3019e0f8e25f
adrianhdez2 <cesarahh3@gmail.com>
2024-04-03 23:18:50 -0600

    Lista of product of user

---
+++ src/components/user/ProductList.jsx
@@ -0,0 +1,33 @@
+import { ArrowDownWideNarrow, LayoutGrid, Plus } from "lucide-react"
+import { dataProducts } from "../../constans/data"
+import CardProductUser from "../CardProductUser"
+
+
+function ProductList() {
+
+  return (
+    <section className="user_page_rigth_container">
+      <header className="user_page_rigth_header">
+        <LayoutGrid size={30} className="user_page_icon" />
+        <div className="user_page_rigth_header_container">
+          <ArrowDownWideNarrow size={15} className="icon_header" />
+          <select name="order" id="order">
+            <option value="">A-Z</option>
+            <option value="">Recientes</option>
+            <option value="">Antigüos</option>
+          </select>
+        </div>
+      </header>
+      <article className="user_page_right_products">
+        {
+          dataProducts.map((item) => (
+            <CardProductUser key={item.id} product={item}/>
+          ))
+        }
+      </article>
+      <button className="user_page_right_add_product"><Plus size={30}/></button>
+    </section>
+  )
+}
+
+export default ProductList
