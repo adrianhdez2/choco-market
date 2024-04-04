cb7229c08de4fbfe19d58089059735336a94338f
adrianhdez2 <cesarahh3@gmail.com>
2024-04-03 19:35:05 -0600

    Add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,35 @@ import Details from './pages/Details'
 import ProductsUser from './pages/ProductsUser'
 import Cart from './pages/Cart'
 import { CartProvider } from './components/context/cart'
+import User from './pages/User'
+import Purchases from './components/user/Purchases'
+import Settings from './components/user/Settings'
+import Statistics from './components/user/Statistics'
+import HomeUser from './components/user/HomeUser'
 
 function App() {
 
   return (
     <CartProvider>
+
       <Header />
-
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/products/:user' element={<ProductsUser />} />
-        <Route path='/products/details/:id' element={<Details />} />
-        <Route path='/*' element={<Error />} />
+        <Route path='/' Component={Home} />
+        <Route path='/login' Component={Login} />
+        <Route path='/signup' Component={SignUp} />
+        <Route path='/cart' Component={Cart} />
+        <Route path='/products/:user' Component={ProductsUser} />
+        <Route path='/products/details/:id' Component={Details} />
+        <Route path='/*' Component={Error} />
+        <Route path='/user' Component={User}>
+          <Route path='/user/home' Component={HomeUser} />
+          <Route path='/user/compras' Component={Purchases} />
+          <Route path='/user/estadisticas' Component={Statistics} />
+          <Route path='/user/settings' Component={Settings} />
+        </Route>
       </Routes>
-
       <Footer />
+
     </CartProvider>
   )
 }
