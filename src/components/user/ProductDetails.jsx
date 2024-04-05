468568041076d62b5720ec468d78d5bc98dace44
adrianhdez2 <cesarahh3@gmail.com>
2024-04-04 22:48:01 -0600

    Validate product id

--- src/components/user/ProductDetails.jsx
+++ src/components/user/ProductDetails.jsx
@@ -1,11 +1,23 @@
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
+import { dataProducts } from "../../constans/data";
 
 function ProductDetails() {
-
+    const navigate = useNavigate()
     const { idProducto } = useParams()
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        let newProduct = dataProducts.find(product => product.id_unico === idProducto)
+        if (newProduct) {
+            setProduct(newProduct);
+        } else {
+            navigate('/user/productos')
+        }
+    }, [idProducto, navigate]);
 
     return (
-        <div>Detalles del producto {idProducto}</div>
+        <div>Detalles del producto {product.name}</div>
     )
 }
 
