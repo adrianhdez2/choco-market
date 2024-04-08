c2ddd6381b1fb8ed1c13e7797659dec18c1cd6e3
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-04-07 21:37:11 -0600

    Update menu

--- src/components/portals/Menu.jsx
+++ src/components/portals/Menu.jsx
@@ -19,19 +19,19 @@ function Menu({ classActive, setIsShow }) {
                 <X size={24} className="menu_icon" />
             </button>
             <nav className='header_menu'>
-                <div className='header_menu_search'>
-                    <input type="text" name="search" id="search" placeholder='Botanitas, pastel, dulces...' autoComplete='off' />
-                    <button type='button' className='header_menu_btnSearch'>
+                <form action="/search" className='header_menu_search'>
+                    <input type="text" name="q" id="search" placeholder='Botanitas, pastel, dulces...' autoComplete='off' />
+                    <button type='submit' className='header_menu_btnSearch'>
                         <Search size={24} />
                     </button>
-                </div>
+                </form>
                 <div className='header_menu_filters'>
-                    <a href="#">Comida</a>
-                    <a href="#">Bebidas</a>
-                    <a href="#">Accesorios</a>
-                    <a href="#">Dulceria</a>
-                    <a href="#">Postres</a>
-                    <a href="#">Más</a>
+                    <a className="nav_filter" href="/search?c=comida">Comida</a>
+                    <a className="nav_filter" href="/search?c=bebidas">Bebidas</a>
+                    <a className="nav_filter" href="/search?c=accesorios">Accesorios</a>
+                    <a className="nav_filter" href="/search?c=dulceria">Dulceria</a>
+                    <a className="nav_filter" href="/search?c=postres">Postres</a>
+                    <a className="nav_filter" href="/search?c=all">Todos</a>
                 </div>
             </nav>
         </section>
