30702ee67a42e34e779c7e9d2e3aeca336c53083
adrianhdez2 <cesarahh3@gmail.com>
2024-04-08 14:36:02 -0600

    Create purchases page

--- src/pages/user/Purchases.jsx
+++ src/pages/user/Purchases.jsx
@@ -1,7 +1,33 @@
+import { ShoppingBasket } from "lucide-react"
+import { Link } from "react-router-dom"
 
 function Purchases() {
   return (
-    <div>Purchases</div>
+    <section className="user_page_rigth_container">
+      <header className="user_page_rigth_header">
+        <ShoppingBasket size={30} className="user_page_icon" />
+      </header>
+      <div className="purchases user_page_rigth_header_container">
+        {
+          Array.from({ length: 4 }).map((item, index) => (
+            <Link to={""} className="purchases_container" key={index}>
+              <div className="purchases_container_img">
+                <img src="/profile.jpg" alt="" />
+              </div>
+              <div className="purchases_container_details">
+                <h4>Mouse</h4>
+                <small>feb-2024</small>
+                <small>1 pza.</small>
+              </div>
+              <div className="purchases_container_total">
+                <small>$ 50.00</small>
+                <small>Total: $ 50.00</small>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
