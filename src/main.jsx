fe4659c69c4d10133923ba4eb96e59859ecf529c
adrianhdez2 <cesarahh3@gmail.com>
2024-05-22 13:34:18 -0600

    Remove auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
-
-const api_key = import.meta.env.VITE_SECRET_API_KEY || process.env.VITE_SECRET_API_KEY
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={api_key}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,6 @@ import { useState } from "react"
 import { User, KeyRound } from "lucide-react"
 import { Link } from "react-router-dom"
 import InputField from '../components/form/InputField'
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from "jwt-decode";
 
 function Login() {
   const [isShow, setIsShow] = useState(false)
@@ -39,12 +37,6 @@ function Login() {
     <div className="container_form_general">
       <div className="login container_form">
         <h3 className="title_form">Iniciar sesión</h3>
-        <GoogleLogin
-          onSuccess={credentialResponse => {
-            const decoded = jwtDecode(credentialResponse?.credential);
-            console.log(decoded);
-          }}
-        />
         <form id="login-signup-form" onSubmit={handleSubmit}>
           <InputField type={"text"} name="matricula" value={values.matricula} placeholder="Matricula" onChange={handleValues} icon={User} />
 
