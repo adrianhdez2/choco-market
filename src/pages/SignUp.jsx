e24d92519fdb9e373dacd42fb1d2033e8c93fd43
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-05-24 21:05:45 -0600

    Validate errors

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import { User, KeyRound, Mail } from "lucide-react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import InputField from "../components/form/InputField"
 import Button from "../components/form/Button"
@@ -16,6 +16,9 @@ function SignUp() {
     passwordConfirm: ''
   })
 
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
   const handleValues = (e) => {
     let { target } = e
     const { name, value } = target
@@ -31,32 +34,49 @@ function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    if (values.password === values.passwordConfirm) {
-      await axios.post("http://localhost:8000/api/users/register", values)
-        .then(res => {
-          setValues({
-            names: '',
-            lastnameP: '',
-            lastnameM: '',
-            email: '',
-            password: '',
-            passwordConfirm: ''
-          })
-
-          navigate('/login')
+    try {
+      setLoading(true)
+      const res = await axios.post("http://localhost:8000/api/users/register", values)
+      if (res.status == 200) {
+        setValues({
+          names: '',
+          lastnameP: '',
+          lastnameM: '',
+          email: '',
+          password: '',
+          passwordConfirm: ''
         })
 
-
-    } else {
-      console.error("Las contraseñas no coinciden");
+        navigate('/login')
+      }
+    } catch (err) {
+      setError(err.response?.data?.error || "Error de inicio de sesión");
+      setLoading(false);
     }
   }
+
+  useEffect(() => {
+    if (error) {
+      setError(error);
+      const timer = setTimeout(() => {
+        setError('');
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   return (
     <div className="container_form_general">
       <div className="signup container_form">
         <h3 className="title_form">Registro</h3>
         <form id="login-signup-form" onSubmit={handleSubmit}>
+          {
+            error &&
+            <div className="error_dialog">
+              <small className="error_dialog_text">{error}</small>
+            </div>
+          }
           <div className="container_inputs_signup">
             <InputField type={"text"} name="names" value={values.names} placeholder="Nombre(s)" onChange={handleValues} icon={User} />
             <InputField type={"text"} name="lastnameP" value={values.lastnameP} placeholder="Apellidos paternos" onChange={handleValues} icon={User} />
@@ -69,7 +89,7 @@ function SignUp() {
             <InputField type={"password"} name="password" value={values.password} placeholder="Contraseña" onChange={handleValues} icon={KeyRound} />
             <InputField type={"password"} name="passwordConfirm" value={values.passwordConfirm} placeholder="Confirmar contraseña" onChange={handleValues} icon={KeyRound} />
           </div>
-          <Button loading={false} title={'Registrarme'} />
+          <Button loading={loading} title={'Registrarme'} />
         </form>
         <p className="form_link_question">¿Ya tienes cuenta?
           <Link to={"/login"} id="form_link_account">Entrar</Link>
